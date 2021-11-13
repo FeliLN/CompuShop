719fc6e4682c9a8e96d05450b57a2f498a817840
juliandebra <julian.debra25@gmail.com>
2021-11-13 19:29:47 -0300

    mensaje

--- src/Components/Carta/Carta.jsx
+++ src/Components/Carta/Carta.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Card, Button } from 'react-bootstrap';
+import './Carta.css'
 
-const Carta = () => {
+const Carta = (props) => { //Traigo los atributos del objeto creado en Shop 
    
    const variable = "tuvieja";
    
     return (
-        <div>
-            <Card className="text-center">
-                <Card.Img className="Card.Img" variant="top" src="holder.js/100px180" />
+        <div className="">
+            <Card className="card">
+                {/* Aca le pasé el atributo de img que traigo desde Shop */}
+                <Card.Img className="CardImg" variant="top" src={props.img} /> 
+                {/*  */}
                 <Card.Header>Header</Card.Header>
                 <Card.Body>
                     <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
+                    <Card.Text className="text">
                     Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    {variable}
+                    the card's content.asdasd
+                    {variable} 
                     </Card.Text>
                     <Button variant="primary">Go somewhere</Button>
                 </Card.Body>
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
-
+import './Shop.css';
 import Carta from '../Carta/Carta';
 
 const Shop = () => {
+    
+    const shop = {
+        items : [
+            {id: 1, name: 'Item 1', price: '$1.99', img:'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+            {id: 2, name: 'Item 2', price: '$2.99', img:'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+            {id: 3, name: 'Item 3', price: '$3.99', img: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+            {id: 4, name: 'Item 4', price: '$4.99', img: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+            {id: 5, name: 'Item 5', price: '$5.99', img: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+            {id: 6, name: 'Item 6', price: '$6.99', img: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+            {id: 7, name: 'Item 7', price: '$7.99', img: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+            {id: 8, name: 'Item 8', price: '$8.99', img: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+            {id: 9, name: 'Item 9', price: '$9.99', img: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+            {id: 10, name: 'Item 10', price: '$10.99', img: 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'},
+
+        ]
+    }
+    
+    
+    
     return (
-        <div>
-            <Carta />
-         
+        <div className="Board">
+            <section className="Cartas">
+                {shop.items.map(item => <Carta key={item.id} item={item} img={item.img}/>)}
+            </section>
         </div>
     )
 }
