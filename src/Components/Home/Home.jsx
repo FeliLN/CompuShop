4c5141ff4182d3fd092b80cf2b35939954b3679d
FeliLN <feli4195@gmail.com>
2021-11-18 15:39:54 -0300

    Final changes

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,15 +3,15 @@ import React from 'react'
 
 
 
+
 const Home = () => {
 
 
     return (
         <div>
             <h1>
-         Compu Mundo Hiper Mega Red
-        </h1>
-        
+                Compu Mundo Hiper Mega Red
+            </h1>
         </div>
     )
 }
